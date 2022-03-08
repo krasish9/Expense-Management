fd03570ec52d5057c15c6004244f501e420a3a9c
asish <kumarasish9@gmail.com>
2022-03-08 08:01:33 +0530

    first commit

---
+++ src/test/java/com/codehygeine/Expense_Management/ExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehygeine.Expense_Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
